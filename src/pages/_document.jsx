3e9a63d77f1992894b2d9b7a7cd24a897db47e0e
bregtemundo <bregt@pure-communication.be>
2021-08-25 14:50:27 +0200

    start of prismic site

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -15,7 +15,7 @@ class CustomDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover, user-scalable=no" />
 
           {/* preload important font-face */}
-          {/* <link rel="preload" href="/assets/fonts/merriweather-v25-latin-700.woff2" as="font" type="font/woff2" crossOrigin="anonymous" /> */}
+          <link rel="preload" href="/assets/fonts/merriweather-v25-latin-700.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
 
           {/** Open Graph / Facebook */}
           <meta property="og:type" content="website" />

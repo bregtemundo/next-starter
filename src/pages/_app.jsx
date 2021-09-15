b470d26fa720a2b54046dde15ed0318ff02f1bf8
bregtemundo <bregt@pure-communication.be>
2021-09-15 20:51:41 +0200

    Default <Head> belongs in _app otherwise tags are duplicated when overwriting in page

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -14,10 +14,52 @@ import { SampleProvider } from "contexts/SampleContext";
 import "styles/global";
 import Layout from "../components/Layout/index";
 
+import { apiEndpoint } from "../../prismic-configuration"; // import the endpoint name from where it's defined
+const prismicRepoName = /([a-zA-Z0-9-]+)?(\.cdn)?\.prismic\.io/.exec(apiEndpoint)[1]; //Regex to get repo ID
+
 // Custom Application
 const CustomApp = ({ Component, pageProps }) => (
   <Main>
-    <Head></Head>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+
+      {/* preview */}
+      <script async defer src={`//static.cdn.prismic.io/prismic.js?repo=${prismicRepoName}&new=true`} />
+
+      {/** Primary Meta Tags */}
+      <meta name="title" content="Next starter" />
+      <meta name="description" content="Next starter" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover, user-scalable=no" />
+
+      {/* preload important font-face */}
+      <link rel="preload" href="/assets/fonts/merriweather-v25-latin-700.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+
+      {/** Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="http://localhost:3001" />
+      <meta property="og:title" content="Next starter" />
+      <meta property="og:image" content="/assets/images/share.png" />
+      <meta property="og:description" content="Next starter" />
+
+      {/** Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="http://localhost:3001" />
+      <meta property="twitter:title" content="Next starter" />
+      <meta property="twitter:description" content="Next starter" />
+      <meta property="twitter:image" content="/assets/images/share.png" />
+
+      {/** Icons and theme color */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/icons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/icons/favicon-16x16.png" />
+      <link rel="manifest" href="/assets/images/icons/site.webmanifest" />
+      <link rel="mask-icon" href="/assets/images/icons/safari-pinned-tab.svg" color="#ccff00" />
+      <link rel="shortcut icon" href="/assets/images/icons/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#ccff00" />
+      <meta name="msapplication-config" content="/assets/images/icons/browserconfig.xml" />
+      <meta name="theme-color" content="#ccff00" />
+    </Head>
 
     <Layout>
       <Component {...pageProps} />

7c0f40db001eb9fcabb47136a333c27e7120b5b5
bregtemundo <bregt@pure-communication.be>
2021-09-17 16:21:52 +0200

    added seo fields to primisc content

--- .slicemachine/assets/src/slices/QuoteSlice/index.stories.js
+++ .slicemachine/assets/src/slices/QuoteSlice/index.stories.js
@@ -5,8 +5,8 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"quote_slice","items":[],"primary":{"quotetext":[{"type":"paragraph","text":"Irure minim mollit voluptate quis officia. Nulla culpa consequat esse fugiat proident adipisicing commodo cillum sint esse aliquip tempor enim magna officia. Veniam laborum in nisi ut minim aliqua deserunt tempor nisi pariatur ad exercitation magna ea.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"quote_slice","items":[],"primary":{"quotetext":[{"type":"paragraph","text":"Commodo anim magna cupidatat id. Ipsum et laborum aute tempor. Duis anim pariatur esse excepteur velit deserunt deserunt sint in.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
 
-export const _QuoteReference = () => <MyComponent slice={{"variation":"quoteReference","name":"Quote Reference","slice_type":"quote_slice","items":[],"primary":{"quotetext":[{"type":"paragraph","text":"Exercitation ut et magna id tempor non id. Cupidatat nostrud do mollit sit cupidatat nisi nisi in. Non et nisi nulla consectetur.","spans":[]}],"reference":[{"type":"paragraph","text":"Ea eu aute dolor pariatur fugiat sunt adipisicing aute deserunt voluptate elit amet fugiat officia. Cupidatat enim dolor velit eu occaecat commodo adipisicing ex fugiat laborum.","spans":[]}]},"id":"_QuoteReference"}} />
+export const _QuoteReference = () => <MyComponent slice={{"variation":"quoteReference","name":"Quote Reference","slice_type":"quote_slice","items":[],"primary":{"quotetext":[{"type":"paragraph","text":"Commodo irure tempor eiusmod laboris irure culpa eu ea excepteur eu.","spans":[]}],"reference":[{"type":"paragraph","text":"Irure nulla duis in magna cupidatat aliqua non laborum occaecat aliqua. Dolor pariatur amet minim adipisicing Lorem ex exercitation nostrud eu laboris. Officia in occaecat sunt labore eiusmod nisi.","spans":[]}]},"id":"_QuoteReference"}} />
 _QuoteReference.storyName = 'Quote Reference'
